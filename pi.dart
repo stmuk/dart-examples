d38e74f83c7fb9b11b9b2ac8fa44223d47a7674e
Steve Mynott <steve.mynott@gmail.com>
2017-07-12 13:01:13 +0100

    import

---
+++ pi.dart
@@ -0,0 +1,14 @@
+void main() {
+    var n = 1000000000;
+    var delta = 1.0 / n;
+    var sum=0;
+    var x;
+
+    for ( var i = 1; i <= n; i++ ) {
+        x = (i - 0.5) * delta;
+        sum += 1.0 / (1.0 + x*x);
+    }
+    var pi = 4.0 * delta * sum;
+    print(pi);
+    print((n.toString()).length);
+}
